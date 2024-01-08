66fa094c3c3bb7baed0e6455acb04fc8fd7c29b2
mujatba121 <155416718+mujatba121@users.noreply.github.com>
2024-01-08 04:38:06 -0800

    Add files via upload

---
+++ even and odd.py
@@ -0,0 +1,21 @@
+#make a programme that just print the even,odd numbers from 50 to 100
+#using both for and while loops
+i=50
+while i<=100:
+   print(i)
+   i+=2
+
+  #using for loop
+for i in range(50,101,2):
+   print(i)
+
+#make a programme that just print the odd numbers from 100 to 150
+#using both for and while loops
+t=101
+while t<=150:
+   print(t)
+   t+=2
+   #using for loop
+for t in range(101,150,2):
+   print(t)
+
