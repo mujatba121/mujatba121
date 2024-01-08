5a3472514f31f3214a0987a36d90252153494b3a
mujatba121 <155416718+mujatba121@users.noreply.github.com>
2024-01-08 04:41:56 -0800

    Add files via upload

---
+++ matrix.py
@@ -0,0 +1,12 @@
+matrix=[[1,2],[3,4],8]
+print(matrix[1][1])
+#now some list related questions
+numbers=[1,2,3,4,5,6,7,8,8,8]
+sum=0
+for i in numbers:
+    print(i)
+    if i>=8:
+        sum+=i #it means sum=sum+i
+
+        print("the sum of numbers in list is:",sum)
+
